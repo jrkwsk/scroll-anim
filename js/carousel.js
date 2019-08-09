afe2b06db60fe11efd16cfd2d310a9473b657405
mptu <maptu.office@gmail.com>
2019-08-09 18:03:52 +0200

    commit init

---
+++ js/carousel.js
@@ -0,0 +1,46 @@
+//$(document).ready(function(){
+//  // Activate Carousel
+//  $("#myCarousel").carousel({interval: 1000});
+//    
+//  // Enable Carousel Indicators
+//  $(".item1").click(function(){
+//    $("#myCarousel").carousel(0);
+//  });
+//  $(".item2").click(function(){
+//    $("#myCarousel").carousel(1);
+//  });
+//  $(".item3").click(function(){
+//    $("#myCarousel").carousel(2);
+//  });
+//    
+//  // Enable Carousel Controls
+//  $(".carousel-control-prev").click(function(){
+//    $("#myCarousel").carousel("prev");
+//  });
+//  $(".carousel-control-next").click(function(){
+//    $("#myCarousel").carousel("next");
+//  });
+//
+//
+//  // Activate Carousel
+//  $("#myCarousel2").carousel({interval: 500});
+//    
+//  // Enable Carousel Indicators
+//  $(".item1").click(function(){
+//    $("#myCarousel2").carousel(0);
+//  });
+//  $(".item2").click(function(){
+//    $("#myCarousel2").carousel(1);
+//  });
+//  $(".item3").click(function(){
+//    $("#myCarousel2").carousel(2);
+//  });
+//    
+//  // Enable Carousel Controls
+//  $(".carousel-control-prev").click(function(){
+//    $("#myCarousel2").carousel("prev");
+//  });
+//  $(".carousel-control-next").click(function(){
+//    $("#myCarousel2").carousel("next");
+//  });
+//});
---
+++ js/effects.js
@@ -0,0 +1,3 @@
+var sal = require('sal.js')
+
+
